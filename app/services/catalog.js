ae756bbbcea2f119471b4765728df8340330a0d3
Ram Maddali <ram.maddali@gmail.com>
2020-11-18 18:43:52 -0500

    Update catalog.js

--- app/services/catalog.js
+++ app/services/catalog.js
@@ -9,7 +9,7 @@ angular.module("app")
 		baseUrl = (COOLSTORE_CONFIG.SECURE_API_ENDPOINT.startsWith("https://") ? COOLSTORE_CONFIG.SECURE_API_ENDPOINT : "https://" + COOLSTORE_CONFIG.SECURE_API_ENDPOINT + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1")) + '/api/products';
 	} else {
 		baseUrl = "http://a223e9706d9d0476a868104422eabe4a-1447157672.us-west-2.elb.amazonaws.com"+ '/api/products';
-		alert("url"+baseUrl);
+		
 	}
 
 	factory.getProducts = function() {
